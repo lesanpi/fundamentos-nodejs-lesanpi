9abbee6277defa774d54a761faed996e75034d85
Luis Sanchez <lespinerua@gmail.com>
2020-02-12 05:53:28 -0400

    Conceptos Basicos de NodeJS Completada

--- conceptos/entorno.js
+++ conceptos/entorno.js
@@ -1,2 +1,5 @@
-let saludo = "Hola";
-console.log(saludo);
+let nombre = process.env.NOMBRE || "Sin nombre";
+let web = process.env.WEB || "No tengo web";
+
+console.log(`Hola ${nombre}`);
+console.log(`Mi web es: ${web}`);
